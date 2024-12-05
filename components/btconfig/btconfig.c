1c320eefc5042950ac0d1c0943c280d150925c80
Egyember <gyengezsombor2005@gmail.com>
2024-12-05 16:18:59 +0100

    bt stuff

---
+++ components/btconfig/btconfig.c
@@ -0,0 +1,17 @@
+#include "nvs.h"
+#include "esp_bt.h"
+
+void writeNvs(char* ssid, char* passwd, int channel, int auth){
+	nvs_handle_t nvsHandle;
+	nvs_open("wifi_auth", NVS_READWRITE, &nvsHandle);
+	nvs_set_str(nvsHandle, "ssid", ssid);
+	nvs_set_str(nvsHandle, "passwd", passwd);
+	nvs_set_u8(nvsHandle, "channel", channel);
+	nvs_set_u8(nvsHandle, "authMetod", auth);
+	nvs_commit(nvsHandle);
+	nvs_close(nvsHandle);
+};
+
+void btconfig(){
+
+};
